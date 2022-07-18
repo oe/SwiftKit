d642cdbbaf7ecf537e7c06be7d6c1003fde884b6
Saiya <oklark@outlook.com>
2022-07-18 21:44:58 +0800

    feat: add crawl to http request, make better code composition

--- Sources/SwiftKit/http-request.swift
+++ Sources/SwiftKit/http-request.swift
@@ -6,209 +6,10 @@
 //
 import Combine
 import Foundation
+import WebKit
 
 public enum HTTPRequest {
-  
-  /// HTTP Error
-  public enum RequestError: Error {
-    case urlMissing
-    case invalidURL
-    case malformedBody
-  }
 
-
-  /// HTTP methods
-  public enum Method {
-    case GET
-    case POST
-    case PUT
-    case DELETE
-    case CUSTOM(_ method: String)
-    
-    public func toString() -> String {
-      switch self {
-        case .CUSTOM(let method):
-          return method.uppercased()
-        case .GET:
-          return "GET"
-        case .POST:
-          return "POST"
-        case .PUT:
-          return "PUT"
-        case .DELETE:
-          return "DELETE"
-      }
-    }
-  }
-  
-  /// common http request payload
-  public struct Request {
-    /// URL target
-    public var url: URL?
-    /// request method, default to get
-    public var method: Method?
-    /// query string
-    public var qs: [String: String]?
-    /// http headers
-    public var headers: [String: String]?
-    /// request body (for post/put)
-    public var body: Data?
-    /// use json request, add accept/content-type header automaticlly
-    public var json: Bool?
-    /// timeout in senconds, default to 60s internal
-    public var timeout: TimeInterval?
-    
-    /// init with standard parameters
-    public init(url: URL, method: Method? = nil, qs: [String : String]? = nil, headers: [String : String]? = nil, body: Data? = nil, json: Bool? = nil, timeout: TimeInterval? = nil) {
-      self.url = url
-      self.method = method
-      self.qs = qs
-      self.headers = headers
-      self.body = body
-      self.json = json
-      self.timeout = timeout
-    }
-    
-    
-    /// init with encodable body
-    public init<T: Encodable>(url: URL, method: Method? = nil, qs: [String : String]? = nil, headers: [String : String]? = nil, body: T? = nil, json: Bool? = nil, timeout: TimeInterval? = nil) throws {
-      if let body = body {
-        guard let bodyData = try? JSONEncoder().encode(body) else {
-          throw RequestError.malformedBody
-        }
-        self.init(url: url, method: method, qs: qs, headers: headers, body: bodyData, json: json, timeout: timeout)
-      } else {
-        self.init(url: url, method: method, qs: qs, headers: headers, body: nil, json: json, timeout: timeout)
-      }
-    }
-    
-    /// convert to an URLRequest object which can be used in http request
-    public func toURLRequest() throws -> URLRequest {
-      guard let url = url else {
-        throw RequestError.urlMissing
-      }
-      var urlRequest: URLRequest = URLRequest(url: url)
-      // append qs
-      if let qs = self.qs {
-        var urlComponents = URLComponents(string: url.absoluteString)!
-        if urlComponents.queryItems == nil {
-          urlComponents.queryItems = []
-        }
-        for (key, value) in qs {
-          urlComponents.queryItems!.append(URLQueryItem(name: key, value: value))
-        }
-        urlRequest.url = urlComponents.url
-      }
-      // default to GET if nil
-      urlRequest.httpMethod = (method ?? .GET).toString()
-      
-      if let headers = self.headers {
-        for (key, value) in headers {
-          urlRequest.addValue(value, forHTTPHeaderField: key)
-        }
-      }
-      
-      let usingJson = json == true
-      
-      if let body = self.body {
-        urlRequest.httpBody = body
-        if usingJson {
-          urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        }
-      }
-      if usingJson {
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
-      }
-      
-      return urlRequest
-    }
-  }
-
-  
-  /// URL Response struct
-  public struct Response {
-    let request: Request
-    let data: Data
-    let response: URLResponse
-    let httpResponse: HTTPURLResponse?
-    
-    public init (request: Request, data: Data, response: URLResponse) {
-      self.request = request
-      self.data = data
-      self.response = response
-      if let httpResponse = response as? HTTPURLResponse {
-        self.httpResponse = httpResponse
-      } else {
-        self.httpResponse = nil
-      }
-    }
-    
-    public var status: Int {
-      httpResponse?.statusCode ?? 0
-    }
-    
-    public var url: URL? {
-      response.url
-    }
-    /// is request OK
-    public var isOk: Bool {
-      (200...299).contains(status) || status == 0
-    }
-    
-    public var redirected: Bool {
-      url != request.url
-    }
-    
-    public var statusText: String {
-      HTTPURLResponse.localizedString(forStatusCode: status)
-    }
-    
-    public func getHeader(_ name: String) -> String? {
-      httpResponse?.value(forHTTPHeaderField: name)
-    }
-    
-    public var headers: [AnyHashable: Any]? {
-      httpResponse?.allHeaderFields
-    }
-    
-    public var body: Data {
-      data
-    }
-
-    public func text() -> String {
-      let encoding: String.Encoding!
-      if let textEncodingName = response.textEncodingName {
-        let cfe = CFStringConvertIANACharSetNameToEncoding(textEncodingName as CFString)
-        let se = CFStringConvertEncodingToNSStringEncoding(cfe)
-        encoding = String.Encoding(rawValue: se)
-      } else {
-        encoding = .utf8
-      }
-      return String(data: data, encoding: encoding) ?? ""
-    }
-    
-    public func json<T: Decodable>() throws -> T {
-      let decoder = JSONDecoder()
-      return try decoder.decode(T.self, from: data)
-    }
-    
-    /// decode to struct with custom decoder
-    /// - Parameter getDecoder function to get that decoder
-    /// - Returns: parsed struct
-    ///
-    ///  due to latest swift (5.7) cann't infer type from a default parameter ( https://forums.swift.org/t/generic-parameter-d-could-not-be-inferred/58696 )
-    ///       this method cann't be written in following pattern and replace the previous one
-    ///       `public func json<T: Decodable, D: TopLevelDecoder>(_ getDecoder: (() -> D) = { JSONDecoder() } )  throws -> T where D.Input == Data {`
-    public func json<T: Decodable, D: TopLevelDecoder>(decoder getDecoder: (() -> D) )  throws -> T where D.Input == Data {
-      let decoder = getDecoder()
-      return try decoder.decode(T.self, from: data)
-    }
-    
-    public func blob() -> Data {
-      data
-    }
-  }
-  
   /// request with an url string
   ///
   /// ```swift
@@ -339,6 +140,253 @@ public enum HTTPRequest {
     let resp = try await fetch(payload)
     return try resp.json(decoder: getDecoder)
   }
+}
 
 
+/// extension for request and response
+public extension HTTPRequest {
+  /// common http request payload
+  struct Request {
+    /// URL target
+    public var url: URL?
+    /// request method, default to get
+    public var method: Method?
+    /// query string
+    public var qs: [String: String]?
+    /// http headers
+    public var headers: [String: String]?
+    /// request body (for post/put)
+    public var body: Data?
+    /// use json request, add accept/content-type header automaticlly
+    public var json: Bool?
+    /// timeout in senconds, default to 60s internal
+    public var timeout: TimeInterval?
+    
+    /// init with standard parameters
+    public init(url: URL? = nil, method: Method? = nil, qs: [String : String]? = nil, headers: [String : String]? = nil, body: Data? = nil, json: Bool? = nil, timeout: TimeInterval? = nil) {
+      self.url = url
+      self.method = method
+      self.qs = qs
+      self.headers = headers
+      self.body = body
+      self.json = json
+      self.timeout = timeout
+    }
+    
+    
+    /// init with encodable body
+    public init<T: Encodable>(url: URL? = nil, method: Method? = nil, qs: [String : String]? = nil, headers: [String : String]? = nil, body: T? = nil, json: Bool? = nil, timeout: TimeInterval? = nil) throws {
+      if let body = body {
+        guard let bodyData = try? JSONEncoder().encode(body) else {
+          throw RequestError.malformedBody
+        }
+        self.init(url: url, method: method, qs: qs, headers: headers, body: bodyData, json: json, timeout: timeout)
+      } else {
+        self.init(url: url, method: method, qs: qs, headers: headers, body: nil, json: json, timeout: timeout)
+      }
+    }
+    
+    /// convert to an URLRequest object which can be used in http request
+    public func toURLRequest() throws -> URLRequest {
+      guard let url = url else {
+        throw RequestError.urlMissing
+      }
+      var urlRequest: URLRequest = URLRequest(url: url)
+      // append qs
+      if let qs = self.qs {
+        var urlComponents = URLComponents(string: url.absoluteString)!
+        if urlComponents.queryItems == nil {
+          urlComponents.queryItems = []
+        }
+        for (key, value) in qs {
+          urlComponents.queryItems!.append(URLQueryItem(name: key, value: value))
+        }
+        urlRequest.url = urlComponents.url
+      }
+      // default to GET if nil
+      urlRequest.httpMethod = (method ?? .GET).toString()
+      
+      if let headers = self.headers {
+        for (key, value) in headers {
+          urlRequest.addValue(value, forHTTPHeaderField: key)
+        }
+      }
+      
+      let usingJson = json == true
+      
+      if let body = self.body {
+        urlRequest.httpBody = body
+        if usingJson {
+          urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        }
+      }
+      if usingJson {
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+      }
+      
+      return urlRequest
+    }
+  }
+
+  /// URL Response struct
+  struct Response {
+    let request: Request
+    let data: Data
+    let response: URLResponse
+    let httpResponse: HTTPURLResponse?
+    
+    public init (request: Request, data: Data, response: URLResponse) {
+      self.request = request
+      self.data = data
+      self.response = response
+      if let httpResponse = response as? HTTPURLResponse {
+        self.httpResponse = httpResponse
+      } else {
+        self.httpResponse = nil
+      }
+    }
+    
+    public var status: Int {
+      httpResponse?.statusCode ?? 0
+    }
+    
+    public var url: URL? {
+      response.url
+    }
+    /// is request OK
+    public var isOk: Bool {
+      (200...299).contains(status) || status == 0
+    }
+    
+    public var redirected: Bool {
+      url != request.url
+    }
+    
+    public var statusText: String {
+      HTTPURLResponse.localizedString(forStatusCode: status)
+    }
+    
+    public func getHeader(_ name: String) -> String? {
+      httpResponse?.value(forHTTPHeaderField: name)
+    }
+    
+    public var headers: [AnyHashable: Any]? {
+      httpResponse?.allHeaderFields
+    }
+    
+    public var body: Data {
+      data
+    }
+
+    public func text() -> String {
+      let encoding: String.Encoding!
+      if let textEncodingName = response.textEncodingName {
+        let cfe = CFStringConvertIANACharSetNameToEncoding(textEncodingName as CFString)
+        let se = CFStringConvertEncodingToNSStringEncoding(cfe)
+        encoding = String.Encoding(rawValue: se)
+      } else {
+        encoding = .utf8
+      }
+      return String(data: data, encoding: encoding) ?? ""
+    }
+    
+    public func json<T: Decodable>() throws -> T {
+      let decoder = JSONDecoder()
+      return try decoder.decode(T.self, from: data)
+    }
+    
+    /// decode to struct with custom decoder
+    /// - Parameter getDecoder function to get that decoder
+    /// - Returns: parsed struct
+    ///
+    ///  due to latest swift (5.7) cann't infer type from a default parameter ( https://forums.swift.org/t/generic-parameter-d-could-not-be-inferred/58696 )
+    ///       this method cann't be written in following pattern and replace the previous one
+    ///       `public func json<T: Decodable, D: TopLevelDecoder>(_ getDecoder: (() -> D) = { JSONDecoder() } )  throws -> T where D.Input == Data {`
+    public func json<T: Decodable, D: TopLevelDecoder>(decoder getDecoder: (() -> D) )  throws -> T where D.Input == Data {
+      let decoder = getDecoder()
+      return try decoder.decode(T.self, from: data)
+    }
+    
+    public func blob() -> Data {
+      data
+    }
+  }
+  
+}
+
+/// extension for error  and Method
+public extension HTTPRequest {
+  /// HTTP Error
+  enum RequestError: Error {
+    case urlMissing
+    case invalidURL
+    case malformedBody
+  }
+
+  /// HTTP methods
+  enum Method {
+    case GET
+    case POST
+    case PUT
+    case DELETE
+    case CUSTOM(_ method: String)
+    
+    public func toString() -> String {
+      switch self {
+        case .CUSTOM(let method):
+          return method.uppercased()
+        case .GET:
+          return "GET"
+        case .POST:
+          return "POST"
+        case .PUT:
+          return "PUT"
+        case .DELETE:
+          return "DELETE"
+      }
+    }
+  }
+}
+
+/// extension for web crawler
+public extension HTTPRequest {
+  private static var cachedUserAgent = ""
+  
+  
+  /// current device userAgent
+  ///   should access it in a async queue, or it will crash your app
+  static var currentUserAgent: String {
+    if cachedUserAgent.isEmpty {
+      cachedUserAgent = WKWebView().value(forKey: "userAgent") as! String
+    }
+    return cachedUserAgent
+  }
+  
+  
+  /// crawl webpage with current use agent by default
+  /// - Parameters:
+  ///   - url: url string
+  ///   - options: custom request payload
+  /// - Returns: html body
+  static func crawl(_ url: String, _ options: Request? = nil) async throws -> String {
+    var request: Request!
+    if options != nil {
+      request = options!
+      request.url = URL(string: url, relativeTo: options!.url)
+    } else {
+      request = Request(url: URL(string: url))
+    }
+    
+    if request.headers != nil {
+      if !request.headers!.contains(where: { $0.key.lowercased() == "user-agent"}) {
+        request.headers!["User-Agent"] = currentUserAgent
+      }
+    } else {
+      request.headers = ["User-Agent": currentUserAgent]
+    }
+    
+    let response = try await fetch(request)
+    return response.text()
+  }
+  
 }
